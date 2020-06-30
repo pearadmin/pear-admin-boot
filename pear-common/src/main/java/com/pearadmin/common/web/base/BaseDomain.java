6380cc2003352b050579409a6516358f902da52a
就眠仪式 <854085467@qq.com>
2020-06-30 09:49:18 +0800

    移除 BaseDomain 中的无效引用

--- pear-common/src/main/java/com/pearadmin/common/web/base/BaseDomain.java
+++ pear-common/src/main/java/com/pearadmin/common/web/base/BaseDomain.java
@@ -1,14 +1,8 @@
 package com.pearadmin.common.web.base;
 
-import com.alibaba.fastjson.annotation.JSONField;
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Map;
 
 @Data
 public class BaseDomain implements Serializable {
