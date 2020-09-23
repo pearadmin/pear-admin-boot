0305cb60206d45186bfab1ad551a1b65becc54a8
就眠仪式 <854085467@qq.com>
2020-09-24 00:16:04 +0800

    process module --> get png / xml resource file

--- pear-modules/pear-process/src/main/java/com/pearadmin/process/controller/ProDefinedController.java
+++ pear-modules/pear-process/src/main/java/com/pearadmin/process/controller/ProDefinedController.java
@@ -7,9 +7,14 @@ import com.pearadmin.common.web.domain.response.ResultTable;
 import com.pearadmin.process.domain.ProDefined;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,6 +66,47 @@ public class ProDefinedController extends BaseController {
     public Result remove(@PathVariable String deploymentId){
         repositoryService.deleteDeployment(deploymentId,true);
         return Result.success("删除成功");
+    }
+
+
+    private InputStream getProcessDefineResource(String processDefineId, String resourceName) {
+
+        return repositoryService.getResourceAsStream(processDefineId, resourceName);
+    }
+
+    @GetMapping("/png")
+    public void getProcessDefineImge(HttpServletResponse response,
+                                     @RequestParam("definedId") String processDefineId,String resourceName){
+        InputStream inputStream = getProcessDefineResource(processDefineId, resourceName);
+        byte[] bytes = new byte[1024];
+        response.setContentType("image/png");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            while (inputStream.read(bytes) != -1) {
+                outputStream.write(bytes);
+            }
+
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/xml")
+    public void getProcessDefineXML(HttpServletResponse response,
+                                    @RequestParam("definedId") String processDefineId,String resourceName){
+        InputStream inputStream = getProcessDefineResource(processDefineId, resourceName);
+        byte[] bytes = new byte[1024];
+        response.setContentType("text/xml");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            while (inputStream.read(bytes) != -1) {
+                outputStream.write(bytes);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
