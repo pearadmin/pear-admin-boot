7b33417be93f28a2e527369b4f465d3f4a0532e3
就眠仪式 <854085467@qq.com>
2020-10-08 19:12:32 +0800

    Druid 广告

--- pear-common/src/main/java/com/pearadmin/common/config/DruidConfig.java
+++ pear-common/src/main/java/com/pearadmin/common/config/DruidConfig.java
@@ -2,6 +2,8 @@ package com.pearadmin.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.util.Utils;
 import com.pearadmin.common.config.proprety.DruidProperty;
 import com.pearadmin.common.plugins.datasource.DynamicDataSource;
 import com.pearadmin.common.plugins.datasource.enums.DataSourceType;
@@ -9,10 +11,14 @@ import com.pearadmin.common.tools.spring.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.servlet.*;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -76,5 +82,49 @@ public class DruidConfig {
         } catch (Exception e) {
             log.info("datasource switch exception");
         }
+    }
+
+    /**
+     * 去除监控页面底部的广告
+     *
+     * @param properties 备选数据源集合
+     */
+    @Bean
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @ConditionalOnProperty(name = "spring.datasource.druid.statViewServlet.enabled", havingValue = "true")
+    public FilterRegistrationBean removeDruidFilterRegistrationBean(DruidStatProperties properties)
+    {
+        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
+        final String filePath = "support/http/resources/js/common.js";
+        Filter filter = new Filter()
+        {
+            @Override
+            public void init(javax.servlet.FilterConfig filterConfig) throws ServletException
+            {
+            }
+
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                    throws IOException, ServletException
+            {
+                chain.doFilter(request, response);
+                response.resetBuffer();
+                String text = Utils.readFromResource(filePath);
+                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+                response.getWriter().write(text);
+            }
+
+            @Override
+            public void destroy()
+            {
+            }
+        };
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns(commonJsPattern);
+        return registrationBean;
     }
 }
