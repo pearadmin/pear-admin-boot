b4ef6de5837eef3c048911de1ad2ad7fcead856d
就眠仪式 <854085467@qq.com>
2020-10-08 14:31:27 +0800

    完 善 注 释

--- pear-modules/pear-process/src/main/java/com/pearadmin/process/domain/ProModel.java
+++ pear-modules/pear-process/src/main/java/com/pearadmin/process/domain/ProModel.java
@@ -11,12 +11,24 @@ import lombok.Data;
 @Data
 public class ProModel extends BaseDomain {
 
+        /**
+         * 编号
+         * */
         private String id;
 
+        /**
+         * 名称
+         * */
         private String name;
 
+        /**
+         * 标识
+         * */
         private String key;
 
+        /**
+         * 版本
+         * */
         private Integer version;
 
 }
--- pear-modules/pear-process/src/main/java/com/pearadmin/process/param/CreateModelParam.java
+++ pear-modules/pear-process/src/main/java/com/pearadmin/process/param/CreateModelParam.java
@@ -5,12 +5,24 @@ import lombok.Data;
 @Data
 public class CreateModelParam {
 
+   /**
+    * 模板名称
+    * */
    private String name;
 
+   /**
+    * 模板标识
+    * */
    private String key;
 
+   /**
+    * 模板标识
+    * */
    private String description;
 
+   /**
+    * 模板版本
+    * */
    private String version;
 
 }
