1baa102e858c09d8d658a5254b78b0bd08c93127
songanwei <50330690@qq.com>
2020-12-04 17:07:42 +0800

    提交字典

--- pear-entrance/src/main/resources/static/component/pear/module/dictionary.js
+++ pear-entrance/src/main/resources/static/component/pear/module/dictionary.js
@@ -37,6 +37,12 @@ layui.define(['jquery', 'element','form'], function(exports) {
             return loadDictData;
 
         }
+        /**
+         * 根据单个字典值获取字典label
+         * @param value
+         * @param dictCode
+         * @returns {*}
+         */
         this.parseDictText=function(value,dictCode){
             var loadDictData = this.dictListByCode(dictCode);
             for (var j = 0; j < loadDictData.length; j++) {
@@ -46,14 +52,41 @@ layui.define(['jquery', 'element','form'], function(exports) {
             }
             return value;
         }
+        /**
+         * 根据多个code获取字典label,隔开
+         * @param values
+         * @param dictCode
+         */
+        this.parseDictTexts=function(values,dictCode){
+            var loadDictData = this.dictListByCode(dictCode);
+            var texts="";
+            if(values!=null&&values.length>0){
+                var defaultValues=values.split(",");
+                if (defaultValues.length > 0) {
+                    for (var j = 0; j < loadDictData.length; j++) {
+                        for (var i = 0; i < defaultValues.length; i++) {
+                            if (defaultValues[i] === loadDictData[j].dataValue) {
+                                texts+=loadDictData[j].dataLabel+",";
+                            }
+                        }
 
+                    }
+                }
+            }
+            if(texts==null||texts==""){
+                texts=values;
+            }else{
+                texts=texts.substr(0,texts.length-1);
+            }
+            return texts;
+        }
     }
 
     $("select[dict-code]").each(function(){
         var _that = $(this);
         var dictCode = $(this).attr("dict-code");
 
-        var defaultValue = $("[dict-code='"+dictCode+"']").attr("default-value");
+        var defaultValue =  $(this).attr("default-value");
         var loadDictData =dictionary.dictListByCode(dictCode);
         for (var j =0;j<loadDictData.length;j++){
             var flag = false;
@@ -70,5 +103,55 @@ layui.define(['jquery', 'element','form'], function(exports) {
 
     });
 
+    $("div[radio-dict-code]").each(function(){
+        var _that = $(this);
+        var dictCode = $(this).attr("radio-dict-code");
+        var name = $(this).attr("name");
+        var defaultValue =  $(this).attr("default-value");
+        var loadDictData =dictionary.dictListByCode(dictCode);
+        for (var j =0;j<loadDictData.length;j++){
+            var flag = false;
+            if(defaultValue!=null&&defaultValue.length>0) {
+                if (defaultValue === loadDictData[j].dataValue) {
+                    flag = true;
+                }
+            }else if(j==0){
+                flag = true;
+            }
+            if(flag){
+                _that.append("<input type='radio' checked name='"+name+"' value='"+loadDictData[j].dataValue+"' title='"+loadDictData[j].dataLabel+"' >");
+            }else{
+                _that.append("<input type='radio' name='"+name+"' value='"+loadDictData[j].dataValue+"' title='"+loadDictData[j].dataLabel+"' >");
+            }
+        }
+        form.render();
+    });
+    $("div[check-box-dict-code]").each(function(){
+        var _that = $(this);
+        var dictCode = $(this).attr("check-box-dict-code");
+        var name = $(this).attr("name");
+        var defaultValue =  $(this).attr("default-value");
+        var loadDictData =dictionary.dictListByCode(dictCode);
+        for (var j =0;j<loadDictData.length;j++){
+            var flag = false;
+            if(defaultValue!=null&&defaultValue.length>0){
+                var defaultValues=defaultValue.split(",");
+                if(defaultValues.length>0){
+                    for(var i=0;i<defaultValues.length;i++){
+                        if(defaultValues[i] === loadDictData[j].dataValue){
+                            flag = true;
+                            break
+                        }
+                    }
+                }
+            }
+            if(flag){
+                _that.append("<input type='checkbox' name='"+name+"' value='"+loadDictData[j].dataValue+"' checked title='"+loadDictData[j].dataLabel+"'  lay-skin='primary' >");
+            }else{
+                _that.append("<input type='checkbox' name='"+name+"'  value='"+loadDictData[j].dataValue+"' title='"+loadDictData[j].dataLabel+"'  lay-skin='primary' >");
+            }
+        }
+        form.render();
+    });
     exports(MOD_NAME, dictionary);
 });
