7e8ade7978bf075c3962bc1fb1c9417ca8c6d46e
Heiky <hongtaoguo0214@sina.com>
2020-12-17 16:40:53 +0800

    添加HttpSessionUtil

---
+++ pear-entrance/src/main/java/com/pearadmin/security/session/HttpSessionUtil.java
@@ -0,0 +1,45 @@
+package com.pearadmin.security.session;
+
+import com.pearadmin.system.domain.SysUser;
+import org.activiti.engine.impl.util.CollectionUtil;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @Author: Heiky
+ * @Date: 2020/12/17 11:40
+ * @Description:
+ */
+public class HttpSessionUtil {
+
+    /**
+     * 从sessionRegistry中清除session信息
+     *
+     * @param request
+     * @param sessionRegistry
+     */
+    public static void expiredSession(HttpServletRequest request, SessionRegistry sessionRegistry) {
+        SysUser currentUser = (SysUser) request.getSession().getAttribute("currentUser");
+        String sessionId = request.getSession().getId();
+        List<Object> principals = sessionRegistry.getAllPrincipals();
+        for (Object principal : principals) {
+            SysUser userDetails = (SysUser) principal;
+            if (userDetails != null && userDetails.getUsername().equals(currentUser.getUsername())) {
+                List<SessionInformation> sessionInformationList = sessionRegistry.getAllSessions(userDetails, true);
+                if (CollectionUtil.isNotEmpty(sessionInformationList)) {
+                    for (SessionInformation sessionInformation : sessionInformationList) {
+                        if (sessionId.equals(sessionInformation.getSessionId())) {
+                            continue;
+                        }
+                        sessionInformation.expireNow();
+                        sessionRegistry.removeSessionInformation(sessionInformation.getSessionId());
+                    }
+                }
+            }
+        }
+    }
+
+}
