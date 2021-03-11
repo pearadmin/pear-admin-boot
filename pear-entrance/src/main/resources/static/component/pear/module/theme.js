d1f8fd15f9932b6d2a2046f4da6f66acb0591dde
就眠仪式 <854085467@qq.com>
2021-03-11 23:20:12 +0800

    fix theme

--- pear-entrance/src/main/resources/static/component/pear/module/theme.js
+++ pear-entrance/src/main/resources/static/component/pear/module/theme.js
@@ -67,6 +67,8 @@ layui.define(["jquery","layer"], function (exports) {
 		style += 'div[xm-select-skin=normal].xm-form-selected .xm-select, div[xm-select-skin=normal].xm-form-selected .xm-select:hover{border-color:'+color+'!important}'
 		style += '.user-group.button-primary{background-color:'+color+'!important}'
 		style += '.layui-layer-btn a:first-child{border-color:'+color+';background-color:'+color+'!important}'
+		style += '.layui-form-checkbox[lay-skin=primary]:hover i{border-color:'+color+'!important}'
+		style += '.pear-tab-menu .item:hover{background-color:'+color+'!important}'
 
 		let colorPane = $("#pear-admin-color");
 		if(colorPane.length>0){
